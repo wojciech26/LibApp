d33d1e98362cc33c99f76bff8adadee6fe32088c
Wąsikiewicz Wojciech <wojciech.wasikiewicz@microsoft.wsei.edu.pl>
2021-10-16 15:03:37 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibApp_Gr3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
