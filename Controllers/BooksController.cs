d4b5550039530ac5d1203904ed6c8e9e2173b6d0
Wąsikiewicz Wojciech <wojciech.wasikiewicz@microsoft.wsei.edu.pl>
2021-10-16 15:07:24 +0200

    added book mvc

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibApp_Gr3.Models;
+
+namespace LibApp_Gr3.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Random()
+        {
+            var firstBook = new Book() { Name = "English dictionary" };
+            return View(firstBook);
+        }
+    }
+}
